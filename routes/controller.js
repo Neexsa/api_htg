0f90fcea6be5079a11b24b71ad498a877147b4fe
Neexsa <81536167+Neexsa@users.noreply.github.com>
2021-08-03 08:52:47 -0300

    Ajuste pdf

--- routes/controller.js
+++ routes/controller.js
@@ -350,7 +350,7 @@ exports.editarRdo = async (req, res, next) => {
                         format: "A4"
                     }).toStream(function(err, stream){
                         if (err) return res.status(500).send(err)
-                        // stream.pipe(fs.createWriteStream(`${body.dataIDRDO}.pdf`));
+                        stream.pipe(fs.createWriteStream(`${body.dataIDRDO}.pdf`));
                         const params = {
                             s3,
                             Bucket: 'neexsa-htg-pdfs',
